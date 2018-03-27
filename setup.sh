7aeb80734f8df8f00b7c3c118396186750003606
Uri Gorelik <uri.gore@gmail.com>
2018-03-27 10:51:47 -0400

    Added ripgrep config file
    For smart case searching

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 CURR_DIR=`pwd`
 
 ln -sf "$CURR_DIR/zshrc" ~/.zshrc
+ln -sf "$CURR_DIR/ripgreprc" ~/.ripgreprc
 
 for custom_file in $(ls -1 *.zsh); do
   ln -sf "$CURR_DIR/$custom_file" ~/.oh-my-zsh/custom/$custom_file
